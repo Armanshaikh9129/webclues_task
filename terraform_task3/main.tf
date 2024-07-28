eb4e028301968e88c27665d83e69f337ce8b351e
Armanshaikh9129 <155525862+Armanshaikh9129@users.noreply.github.com>
2024-07-28 18:49:09 +0530

    Update main.tf

--- terraform_task3/main.tf
+++ terraform_task3/main.tf
@@ -8,13 +8,13 @@ terraform {
 }
   
 
-# main.tf
+
 
 provider "aws" {
-  region = var.region  # Specify your desired region
+  region = var.region 
 }
 
-# Create a VPC
+
 resource "aws_vpc" "my_vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
@@ -23,7 +23,7 @@ resource "aws_vpc" "my_vpc" {
     Name = var.resource_name
   }
 }
-# Create a subnet
+
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = var.subnet_cidr
@@ -34,7 +34,7 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
-# Create an internet gateway
+
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
   tags = {
@@ -56,13 +56,13 @@ resource "aws_route_table" "my_route_table" {
   }
 }
 
-# Associate the route table with the subnet
+
 resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.my_subnet.id
   route_table_id = aws_route_table.my_route_table.id
 }
 
-# Create a security group
+
 resource "aws_security_group" "web_sg" {
   name        = var.sg_name
   vpc_id      = aws_vpc.my_vpc.id
@@ -86,7 +86,7 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-# Launch an EC2 instance
+
 resource "aws_instance" "web" {
   ami             = var.ami_image  # Replace with a valid AMI ID for your region
   instance_type   = var.instance_size
@@ -101,9 +101,9 @@ resource "aws_instance" "web" {
     provisioner "remote-exec" {
     connection {
       type        = "ssh"
-      user        = "ubuntu"  # Adjust this based on the AMI's default user (e.g., "ubuntu" for Ubuntu AMIs)
-      private_key = file(var.private_key_path)  # Path to your private key file
-      host        = self.public_ip  # Use the instance's public IP address
+      user        = "ubuntu"  
+      private_key = file(var.private_key_path)  
+      host        = self.public_ip  
     }
     
      inline = [
